37ed0be7fdf44c858e2fe2bc74569550df4c2dcd
Ida Thorburn <idtho@student.chalmers.se>
2025-02-19 13:36:40 +0100

    lab1

---
+++ Car.java
@@ -0,0 +1,115 @@
+import java.awt.*;
+
+public abstract class Car implements Moveable{
+    private int nrDoors; // Number of doors on the car
+    private double length; // Length of car
+    private double enginePower; // Engine power of the car
+    private double currentSpeed; // The current speed of the car
+    private Color color; // Color of the car
+    private String modelName; // The car model name
+    private Point position;
+    private double direction; // direction in degrees
+
+    public Car(int nrDoors, double length,double enginePower, Color color, String modelName) {
+        this.nrDoors = nrDoors;
+        this.length = length;
+        this.enginePower = enginePower;
+        this.currentSpeed = 0; // Cars are stationary when created
+        this.color = color;
+        this.modelName = modelName;
+        this.position = new Point(0, 0);
+        this.direction = 0;
+    }
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    private void setCurrentSpeed(double speed) {
+        this.currentSpeed = Math.max(0, Math.min(speed, enginePower));
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void startEngine() {
+        setCurrentSpeed(0.1);
+    }
+
+    public void stopEngine() {
+        setCurrentSpeed(0);
+    }
+
+    public abstract double speedFactor();
+
+    private void incrementSpeed(double amount) {
+        setCurrentSpeed(getCurrentSpeed() + speedFactor() * amount);
+    }
+
+    private void decrementSpeed(double amount) {
+        setCurrentSpeed(getCurrentSpeed() - speedFactor() * amount);
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;}
+
+    public Point getPosition() {
+        return position;}
+
+    private void setDirection(double degrees) {
+        this.direction = (degrees % 360 + 360) % 360;}
+
+    public double getDirection() {
+        return direction;}
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        if (amount < 0 || amount > 1) {
+            throw new IllegalArgumentException("Amount must be between 0 and 1");
+        }
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        if (amount < 0 || amount > 1) {
+            throw new IllegalArgumentException("Amount must be between 0 and 1");
+        }
+        decrementSpeed(amount);
+    }
+
+    @Override
+    public void move() {
+        double radians = Math.toRadians(getDirection());
+        int newX = (int) (getPosition().x + Math.cos(radians) * getCurrentSpeed());
+        int newY = (int) (getPosition().y + Math.sin(radians) * getCurrentSpeed());
+        setPosition(new Point(newX, newY));
+    }
+
+    @Override
+    public void turnLeft() {
+        setDirection(getDirection() - 90);
+    }
+
+    @Override
+    public void turnRight() {
+        setDirection(getDirection() + 90);
+    }
+}
---
+++ Volvo240.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Volvo240 extends Car {
+
+    private final static double trimFactor = 1.25;
+
+    
+    public Volvo240(){
+        super(4, 4.8, 100, Color.black, "Volvo240");
+        stopEngine();
+    }
+    
+    public double speedFactor(){
+        return getEnginePower() * 0.01 * trimFactor;
+    }
+}
